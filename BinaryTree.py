95e49bf1deea83178423ed5af4b6b9556f1dafb4
Jao42 <joaovictormedeiros41@gmail.com>
2023-08-12 13:13:45 -0300

    formatando saida

--- BinaryTree.py
+++ BinaryTree.py
@@ -7,29 +7,32 @@ class Node:
 class BinaryTree:
   def __init__(self, root):
     self.root = root
-  def in_order(self, node=''):
+  def in_order(self, node='', camada=0):
     if node == None:
       return ''
     if node == '':
       node = self.root
-    self.in_order(node.left)
-    print(node.value, end=' ')
-    self.in_order(node.right)
+    camada_atual = camada + 1
+    self.in_order(node.left, camada_atual)
+    print(('\t' * camada) + f'{node.value:4d}')
+    self.in_order(node.right, camada_atual)
 
-  def pre_order(self, node=''):
+  def pre_order(self, node='', camada=0):
     if node == None:
       return ''
     if node == '':
       node = self.root
-    print(node.value, end=' ')
-    self.pre_order(node.left)
-    self.pre_order(node.right)
+    camada_atual = camada + 1
+    print(('\t' * camada) + f'{node.value:4d}')
+    self.pre_order(node.left, camada_atual)
+    self.pre_order(node.right, camada_atual)
 
-  def post_order(self, node=''):
+  def post_order(self, node='', camada=0):
     if node == None:
       return ''
     if node == '':
       node = self.root
-    self.post_order(node.left)
-    self.post_order(node.right)
-    print(node.value, end=' ')
+    camada_atual = camada + 1
+    self.post_order(node.left, camada_atual)
+    self.post_order(node.right, camada_atual)
+    print(('\t' * camada) + f'{node.value:4d}')
