200866ca13d4fd623a7cadbecdc3acdc6bbf977d
Jao42 <joaovictormedeiros41@gmail.com>
2023-08-12 11:55:29 -0300

    removendo exemplo da classe BinaryTree

--- BinaryTree.py
+++ BinaryTree.py
@@ -33,18 +33,3 @@ class BinaryTree:
     self.post_order(node.left)
     self.post_order(node.right)
     print(node.value, end=' ')
-
-root = Node(1)
-root.left = Node(2)
-root.left.left = Node(4)
-root.left.right = Node(5)
-root.right = Node(3)
-root.right.left = Node(6)
-root.right.right = Node(7)
-binary_tree = BinaryTree(root)
-
-binary_tree.in_order()
-print('\n')
-binary_tree.pre_order()
-print('\n')
-binary_tree.post_order()
